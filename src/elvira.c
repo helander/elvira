b7a77fb63184cfb19ffe1bd662cf075a767a6565
Lars-Erik Helander <lehswe@gmail.com>
2025-05-26 11:04:10 +0200

    cleanup

--- src/elvira.c
+++ src/elvira.c
@@ -31,32 +31,6 @@ char *read_stream_to_string(FILE *stream) {
    return data;
 }
 
-
-//struct pw_thread_loop *master_loop;
-
-/*
-static void do_quit(void *userdata, int signal_number) {
-   printf("\nTerminate elvira\n");
-   fflush(stdout);
-   pw_thread_loop_stop(master_loop);
-   pw_thread_loop_destroy(master_loop);
-   controller_destroy();
-   pw_deinit();
-   printf("\n elvira terminated\n");
-   fflush(stdout);
-   exit(0);
-}
-
-
-   //   master_loop = pw_thread_loop_new("master", NULL);
-   //   pw_loop_add_signal(pw_thread_loop_get_loop(master_loop), SIGINT, do_quit, "master int");
-   //   pw_loop_add_signal(pw_thread_loop_get_loop(master_loop), SIGTERM, do_quit, "master term");
-   //   pw_thread_loop_start(master_loop);
-
-
-
-*/
-
 int main(int argc, char **argv) {
    gtk_init(&argc, &argv);
    pw_init(&argc, &argv);
