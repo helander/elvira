cb427b5cc7cbb9d37348f91232bbfbe2cd96ac7d
Lars-Erik Helander <lehswe@gmail.com>
2025-05-27 16:13:47 +0200

    create /tmp/elvira

--- src/elvira.c
+++ src/elvira.c
@@ -1,10 +1,16 @@
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <gtk/gtk.h>
 #include <pipewire/pipewire.h>
-#include <stdio.h>
 
 #include "controller/controller.h"
 #include "set.h"
 #include "utils/constants.h"
+
+
+
+
 
 static char *empty_set = "{\"set\": \"\", \"engines\": []}";
 
@@ -57,6 +63,9 @@ int main(int argc, char **argv) {
       fprintf(stderr, "\nNo file content available, use empty set.");
       json_text = empty_set;
    }
+
+   // Potentially used when creating presets, so create it here in case
+   mkdir("/tmp/elvira", 0777);
 
    EngineSet *engines = engineset_parse(json_text);
    if (json_text != empty_set) free(json_text);
